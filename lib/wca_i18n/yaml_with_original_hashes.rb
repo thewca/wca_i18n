a7caafb019c7ac6d599a39c337d8c61029c3bdd9
Philippe Virouleau <philippe.44@gmail.com>
2018-09-01 12:12:28 +0200

    Code review fixes

--- lib/wca_i18n/yaml_with_original_hashes.rb
+++ lib/wca_i18n/yaml_with_original_hashes.rb
@@ -2,6 +2,7 @@ require "psych"
 
 module WcaI18n
   PLURALIZATION_KEYS = %w(zero one two few many other).freeze
+  ORIGINAL_HASH_TAG = "#original_hash: ".freeze
   TranslatedLeaf = Struct.new(:translated, :original_hash)
 
   # Re-implement some parts of the ToRuby emitter to inject our TranslatedLeaf where needs be
@@ -12,9 +13,9 @@ module WcaI18n
       new(scanner, class_loader, original_hashes_map)
     end
 
-    def initialize(ss, cl, original_hashes_map)
-      super(ss, cl)
-      @original_hashes_map = original_hashes_map
+    def initialize(*args, original_hashes_by_line)
+      super(*args)
+      @original_hashes_by_line = original_hashes_by_line
     end
 
     def pluralization_map?(v)
@@ -59,12 +60,12 @@ module WcaI18n
           if v.is_a?(Psych::Nodes::Scalar) && !WcaI18n::PLURALIZATION_KEYS.include?(key)
             # For scalar value, the start line registered is the correct line
             # We assume that the '#original_hash: ' comment comes on the line before.
-            original_hash = @original_hashes_map.delete(v.start_line - 1)
+            original_hash = @original_hashes_by_line.delete(v.start_line - 1)
             val = WcaI18n::TranslatedLeaf.new(val, original_hash)
           end
           if pluralization_map?(v)
             # For mappings, the start line registered is the line of the first key/value!
-            original_hash = @original_hashes_map.delete(v.start_line - 2)
+            original_hash = @original_hashes_by_line.delete(v.start_line - 2)
             val = WcaI18n::TranslatedLeaf.new(val, original_hash)
           end
           hash[key] = val
@@ -79,22 +80,22 @@ module WcaI18n
   class YAMLWithOriginalHashes < Struct.new(:comment, :value)
     def self.parse(text)
       tree = Psych.parser.parse(text)
-      emitter = ToRubyEmitter.create(_original_hashes_map_from_text(text))
+      emitter = ToRubyEmitter.create(_original_hashes_by_line_from_text(text))
       # Not sure why, but "accept" returns an array with one element.
       # Probably because the root of the YAML is a sequence by default?
       emitter.accept(tree.handler.root).first
     end
 
-    def self._original_hashes_map_from_text(text)
-      original_hashes_map = {}
-      # Build a hash mapping a line number to its comment
-      text.each_line.with_index do |line, index|
-        stripped_line = line.strip
-        if stripped_line.start_with?("#original_hash: ")
-          original_hashes_map[index] = stripped_line[16..-1]
+    def self._original_hashes_by_line_from_text(text)
+      {}.tap do |hash|
+        # Build a hash mapping a line number to its comment
+        text.each_line.with_index do |line, index|
+          stripped_line = line.strip
+          if stripped_line.start_with?(ORIGINAL_HASH_TAG)
+            hash[index] = stripped_line[ORIGINAL_HASH_TAG.length..-1]
+          end
         end
       end
-      original_hashes_map
     end
   end
 end
